150758f1786bd789d86a7d81150e53176bd82ec5
Piotr Pawlica <piotr.pawlica@codeconcept.pl>
2020-08-25 12:54:41 +0200

    Date Provider / useDate custom Hook added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './styles/main.css';
 import Calendar from "./components/Calendar";
+import { DateProvider } from "./utilities/DateProvider";
 
 function App() {
   return (
-      <Calendar />
+      <DateProvider>
+        <Calendar />
+      </DateProvider>
   );
 }
 
