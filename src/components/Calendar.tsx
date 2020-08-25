79004b104fee3b3dfc9fa6ed13a1c8f64816efeb
Piotr Pawlica <piotr.pawlica@codeconcept.pl>
2020-08-25 13:24:28 +0200

    Week Days added

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navigation from "./Navigation";
+import WeekDays from "./WeekDays";
 
 const Calendar = () => {
     return (
         <div className="font-body h-screen p-3 md:p-6">
-            <Navigation/>
+            <Navigation />
+            <WeekDays />
         </div>
     );
 };
