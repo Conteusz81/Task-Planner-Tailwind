d83ccb1ca36d3eecd0bea850e73eac38617bf212
Piotr Pawlica <piotr.pawlica@codeconcept.pl>
2020-08-31 15:32:32 +0200

    tailwind.config max width added

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,9 @@ module.exports = {
       gridTemplateRows: {
         'days': 'repeat(6, minmax(5rem, 1fr))',
         'main': 'auto auto 1fr',
-        'nav': 'auto 1fr',
+      },
+      maxWidth: {
+        '90p': '90%',
       }
     },
   },
